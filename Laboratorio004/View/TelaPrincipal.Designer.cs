a43dd34f0aa2446b2b7b34eb956c81e7bc46905c
giovanni.ssilva7 <giovanni.ssilva7@SBC0505232W10-1>
2022-04-28 15:57:31 -0300

    Adicionar arquivos de projeto.

---
+++ Laboratorio004/View/TelaPrincipal.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Laboratorio004.View
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.tbxNumero = new System.Windows.Forms.TextBox();
+            this.tbnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.Location = new System.Drawing.Point(83, 38);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(112, 17);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Digite um Número";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResultado.Location = new System.Drawing.Point(83, 99);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(286, 101);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResultado.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbxNumero
+            // 
+            this.tbxNumero.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.tbxNumero.Location = new System.Drawing.Point(83, 56);
+            this.tbxNumero.Name = "tbxNumero";
+            this.tbxNumero.Size = new System.Drawing.Size(144, 25);
+            this.tbxNumero.TabIndex = 2;
+            this.tbxNumero.TextChanged += new System.EventHandler(this.tbxNumero_TextChanged);
+            // 
+            // tbnCalcular
+            // 
+            this.tbnCalcular.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.tbnCalcular.Location = new System.Drawing.Point(268, 56);
+            this.tbnCalcular.Name = "tbnCalcular";
+            this.tbnCalcular.Size = new System.Drawing.Size(101, 23);
+            this.tbnCalcular.TabIndex = 3;
+            this.tbnCalcular.Text = "Calcular";
+            this.tbnCalcular.UseVisualStyleBackColor = true;
+            this.tbnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 272);
+            this.Controls.Add(this.tbnCalcular);
+            this.Controls.Add(this.tbxNumero);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblTitulo);
+            this.KeyPreview = true;
+            this.Name = "TelaPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Calculo Quadradro";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TelaPrincipal_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox tbxNumero;
+        private System.Windows.Forms.Button tbnCalcular;
+    }
+}
---
+++ Laboratorio004/View/TelaPrincipal.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio004.View
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbxNumero.Text == "")
+            {
+                MessageBox.Show("Insira um Número");
+
+            }
+            else
+            {
+                int numero = 0;
+                int quadrado = 0;
+
+                numero = Convert.ToInt32(tbxNumero.Text);
+
+                quadrado = numero * numero;
+
+                lblResultado.Text ="O número " +numero + " ao quadrado é " + quadrado.ToString();
+                tbxNumero.Focus();
+                tbxNumero.SelectAll();
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TelaPrincipal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            if(e.KeyChar == 13)
+            {
+                tbnCalcular.PerformClick();
+            }
+        }
+
+        private void tbxNumero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
